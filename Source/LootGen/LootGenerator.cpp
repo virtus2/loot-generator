441fc097af6dd1cba0b2f10d0c3b2e6c1c37b921
Kim Hyun Deok <khd1323@naver.com>
2024-01-15 23:43:19 +0900

    Add Loot generator and Data table

---
+++ Source/LootGen/LootGenerator.cpp
@@ -0,0 +1,5 @@
+﻿// Copyright © 2023. KIM HYUN DEOK. All rights reserved.
+
+
+#include "LootGenerator.h"
+
---
+++ Source/LootGen/LootGenerator.h
@@ -0,0 +1,18 @@
+﻿// Copyright © 2023. KIM HYUN DEOK. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LootGenerator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOTGEN_API ULootGenerator : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+
+};
